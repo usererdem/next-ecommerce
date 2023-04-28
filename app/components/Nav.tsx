935ae382292d8a816fae32997436aa3f161fbd62
usererdem <erdemelvan35@gmail.com>
2023-04-28 17:21:15 +0300

    State Management with Zustand Added Cart icon andshowcasing  Cart item amount

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -4,14 +4,24 @@ import { Session } from "next-auth";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
+import Cart from "./Cart";
+import { useCartStore } from "@/store";
+import { AiFillShopping } from "react-icons/ai";
 
 export default function Nav({ user }: Session) {
+  const cartStore = useCartStore();
   return (
-    <nav className='flex justify-between items-center py-8'>
+    <nav className='flex justify-between items-center py-12'>
       <Link href={"/"}>
         <h1>Styled</h1>
       </Link>
       <ul className='flex items-center gap-12'>
+        <li className='flex items-center text-3xl relative cursor-pointer'>
+          <AiFillShopping />
+          <span className='bg-teal-700 text-white text-sm font-bold w-5 h-5 rounded-full absolute left-4 bottom-4 flex items-center justify-center'>
+            {cartStore.cart.length}
+          </span>
+        </li>
         {/* If the use is not signed in */}
         {!user && (
           <li className='bg-teal-600 text-white py-2 px-4 rounded-md'>
@@ -23,13 +33,14 @@ export default function Nav({ user }: Session) {
             <Image
               src={user?.image as string}
               alt={user.name as string}
-              width={48}
-              height={48}
+              width={36}
+              height={36}
               className='rounded-full'
             />
           </li>
         )}
       </ul>
+      {cartStore.isOpen && <Cart />}
     </nav>
   );
 }
