7b3e6abaf688e27da8d25f6d3d841f40dfb9b43c
usererdem <erdemelvan35@gmail.com>
2023-05-02 16:07:10 +0300

    trying to fix add to cart bug

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,62 +1,61 @@
-import { PrismaClient } from "@prisma/client";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../../pages/api/auth/[...nextauth]";
-import formatPrice from "@/util/PriceFormat";
-import Image from "next/image";
+"use client"
 
-export const revalidate = 0;
+import formatPrice from "@/util/PriceFormat"
+import Image from "next/image"
+import { useEffect, useState } from "react"
+import { motion } from "framer-motion"
 
-const fetchOrders = async () => {
-  const prisma = new PrismaClient();
-  const user = await getServerSession(authOptions);
-  if (!user) {
-    return null;
+export default function Dashboard() {
+  const [orders, setOrders] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const fetchOrders = async () => {
+    const res = await fetch("/api/get-orders")
+    const data = await res.json()
+    return data
   }
-  const orders = await prisma.order.findMany({
-    where: {
-      userId: user?.user?.id,
-      status: "complete",
-    },
-    include: {
-      products: true,
-    },
-  });
-  return orders;
-};
-
-export default async function Dashboard() {
-  const orders = await fetchOrders();
-  if (orders === null)
-    return <div>You need to be logged in to view your orders</div>;
-  if (orders.length === 0) {
-    <div>
-      <h1>No orders</h1>
-    </div>;
-  }
+  useEffect(() => {
+    fetchOrders()
+      .then((data) => {
+        setOrders(data)
+        setLoading(false)
+      })
+      .catch((err) => {
+        setError(err)
+        setLoading(false)
+      })
+  }, [])
+  console.log(orders)
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {error}</p>
   return (
-    <div>
-      <div>
+    <motion.div layout>
+      <motion.div layout initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
         {orders.map((order) => (
-          <div key={order.id} className='rounded-lg p-8 my-12 bg-base-200'>
-            <h2 className='text-xs font-medium'>Order reference: {order.id}</h2>
-            <p className='text-md py-2'>
-              Status:{" "}
+          <div
+            key={order.id}
+            className="rounded-lg p-8 my-4 space-y-2 bg-base-200"
+          >
+            <h2 className="text-xs font-medium">Order reference: {order.id}</h2>
+            <p className="text-xs">
+              Status:
               <span
                 className={`${
                   order.status === "complete" ? "bg-teal-500" : "bg-orange-500"
-                } text-white py-1 rounded-md px-2 mx-2 text-sm`}>
+                } text-white py-1 rounded-md px-2 mx-2 text-xs`}
+              >
                 {order.status}
               </span>
             </p>
-            <p className='text-xs'>
-              Time: {new Date(order.createdDate).toString()}{" "}
-            </p>
 
-            <div className='text-sm lg:flex items-center gap-4'>
+            <p className="text-xs">
+              Time: {new Date(order.createdDate).toString()}
+            </p>
+            <div className="text-sm lg:flex items-center  gap-4">
               {order.products.map((product) => (
-                <div className='py-2' key={product.id}>
-                  <h2 className='py-2'>{product.name}</h2>
-                  <div className='flex items-center gap-4'>
+                <div className="py-2" key={product.id}>
+                  <h2 className="py-2">{product.name}</h2>
+                  <div className="flex items-baseline gap-4">
                     <Image
                       src={product.image!}
                       width={36}
@@ -65,16 +64,19 @@ export default async function Dashboard() {
                       priority={true}
                       className="w-auto"
                     />
-                    <p> {formatPrice(product.unit_amount)} </p>
-                    <p>Quantity: {product.quantity} </p>
+                    <p>{formatPrice(product.unit_amount)}</p>
+                    <p>Quantity: {product.quantity}</p>
                   </div>
                 </div>
               ))}
             </div>
-            <p className='font-medium'>Total: {formatPrice(order.amount)}</p>
+            <p className="font-medium py-2">
+              Total: {formatPrice(order.amount)}
+            </p>
           </div>
         ))}
-      </div>
-    </div>
-  );
+      </motion.div>
+    </motion.div>
+  )
 }
+
