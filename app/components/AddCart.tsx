18c232ff4b7c6b9b63194b212be057a446b6ff4a
usererdem <erdemelvan35@gmail.com>
2023-07-03 21:16:44 +0300

    feat: add and remove from cart button added

--- app/components/AddCart.tsx
+++ app/components/AddCart.tsx
@@ -3,6 +3,7 @@
 import { useCartStore } from "@/store"
 import { AddCartType } from "@/types/AddCartType"
 import { useState } from "react"
+import { IoAddCircle, IoRemoveCircle } from "react-icons/io5";
 
 export default function AddCart({
   name,
@@ -23,15 +24,43 @@ export default function AddCart({
   }
 
   return (
-    <>
+    <div className='flex'>
       <button
         onClick={handleAddToCart}
         disabled={added}
-        className="my-4 btn btn-primary w-full"
-      >
+        className='my-4 btn btn-primary w-full'>
         {!added && <span>Add to cart</span>}
         {added && <span>Adding to cart 😄</span>}
       </button>
-    </>
-  )
+
+      {cartStore.cart.map((item) => (
+        <div key={item.id} className='flex bg-base-100 rounded-lg '>
+          <div className='flex'>
+            <button
+              onClick={() =>
+                cartStore.removeProduct({
+                  id: item.id,
+                  image: item.image,
+                  name: item.name,
+                  unit_amount: item.unit_amount,
+                  quantity: item.quantity,
+                })
+              }>
+              <IoRemoveCircle
+                className='mx-4 bg-slate-300 rounded-full border-0 text-primary hover:text-primary-focus'
+                size={32}
+              />
+            </button>
+
+            <button onClick={handleAddToCart} disabled={added}>
+              <IoAddCircle
+                className=' bg-slate-300 rounded-full border-0 text-primary hover:text-primary-focus'
+                size={32}
+              />
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
